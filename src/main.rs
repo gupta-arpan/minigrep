b193a44cd8a9c5dce848e7a098ef2a0d1d48fc1e
Arpan Gupta <arpan@pop-os.localdomain>
2023-10-27 20:18:32 +0530

    Modularity and error handling

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,20 @@
 use std::env;
-use std::fs;
+use std::process;
+
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
     
-    let query = &args[1];
-    let file_path = &args[2];
-
-    println!("Searching for {}", query);
-    println!("In file {}", file_path);
-
-    let contents = fs::read_to_string(file_path)
-    .expect("Something went wrong reading the file");
-
-    println!("Content in file:\n{contents}");
-
-
-
+    if let Err(err) = minigrep::run(config) {
+        println!("Application error: {}", err);
+        process::exit(1);
+    }
 }
+
+
