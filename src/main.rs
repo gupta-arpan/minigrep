882c4567ab7f0ba83d4b50430f5d62aa1f6e1065
Arpan Gupta <arpan@pop-os.localdomain>
2023-10-27 23:02:20 +0530

    Colored errors

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 use std::env;
 use std::process;
-
+use colored::*;
 use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        println!("Problem parsing arguments: {}", err.red().bold());
         process::exit(1);
     });
     
