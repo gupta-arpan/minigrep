a1ce727023cad8cda0b6ad01f68d2b4a93471f78
Arpan Gupta <arpan@pop-os.localdomain>
2023-10-27 14:27:48 +0530

    Reading file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -6,6 +7,14 @@ fn main() {
     let query = &args[1];
     let file_path = &args[2];
 
-    println!("{}", query);
-    println!("{}", file_path);
+    println!("Searching for {}", query);
+    println!("In file {}", file_path);
+
+    let contents = fs::read_to_string(file_path)
+    .expect("Something went wrong reading the file");
+
+    println!("Content in file:\n{contents}");
+
+
+
 }
